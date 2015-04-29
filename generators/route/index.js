9edfb41bb16fc69715eb61057eaa6505975e5540
Stephen Buchanan <stephen@mannmademedia.com>
2015-04-29 14:05:10 +0200

    fixed route yangular ref

--- generators/route/index.js
+++ generators/route/index.js
@@ -11,8 +11,8 @@ module.exports = yeoman.generators.Base.extend({
     this.ctrlname = this._.capitalize(this._.camelize(this._.slugify(
       this._.humanize(this.name))));
 
-    this.composeWith('yangular:view', {args: [this.name]});
-    this.composeWith('yangular:controller', {args: [this.name]});
+    this.composeWith('mmangular:view', {args: [this.name]});
+    this.composeWith('mmangular:controller', {args: [this.name]});
 
     util.wireRoute({
       filename: this.filename,
